1d8ce92f8a963e2963358d0ff3d037dc14024015
Leopold Schlemmer <Leopold.shlemmer@hotmail.de>
2024-05-26 12:20:19 +0200

    Initial Commit

---
+++ MuscleTracker/MuscleTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuscleTrackerApp.swift
+//  MuscleTracker
+//
+//  Created by Genereller Account  on 26.05.24.
+//
+
+import SwiftUI
+
+@main
+struct MuscleTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
